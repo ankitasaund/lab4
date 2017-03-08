01e9c9e8f1a14022e6ebb718c0415594095ae4f5
PatriciaCR <PatriciaCR@Patricias-MacBook-Pro-3.local>
2017-03-08 12:06:57 -0800

    Core Data kinda working
    The entry gets added two times… There’s no section titles yet

--- ListViewapplab2_AnkitaSaundade/AddViewController.swift
+++ ListViewapplab2_AnkitaSaundade/AddViewController.swift
@@ -7,21 +7,35 @@
 //
 
 import UIKit
+import CoreData
 
 class AddViewController: UIViewController {
     
     @IBOutlet var addPlaceName: UITextField!
-    
     @IBOutlet var addPlaceDetail: UITextField!
-
     @IBOutlet var addPlaceImage: UITextField!
     
+    @IBAction func clickedCancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     
-    var newPlaces : ((PlaceObject) -> ())!
+    
+    @IBAction func clickedSave(_ sender: Any) {
+        // Check if name is empty
+        if (addPlaceName.hasText) {
+            self.alertError("Unable to save", msg: "Item name can't be blank.")
+        }
+
+        performSegue(withIdentifier: "saveSegue", sender: sender)
+    }
+    
+    
+    
+    //var newPlaces : ((PlaceObject) -> ())!
+    var newPlaces : PlaceObjectMO!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,27 +45,40 @@ class AddViewController: UIViewController {
     
 
     
-    @IBAction func clickedCancel(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-        
-    }
     
-    
-    @IBAction func clickedSave(_ sender: Any) {
-        newPlaces(PlaceObject(iPlaceName: self.addPlaceName.text!, iPlaceImage: UIImage(named: self.addPlaceImage.text!)!, iPlaceDetail: self.addPlaceDetail.text!, iPlaceChecked: false))
-        
-        self.dismiss(animated: true, completion: nil)
-    }
-   
-    
-    /*
      // MARK: - Navigation
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      // Get the new view controller using segue.destinationViewController.
      // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "saveSegue" {
+            let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).managedObjectContext
+            
+            newPlaces = NSEntityDescription.insertNewObject(forEntityName: "PlaceObject", into: managedObjectContext) as! PlaceObjectMO
+            
+            newPlaces.iPlaceName = addPlaceName.text
+            newPlaces.iPlaceImage = NSData(data: UIImagePNGRepresentation(UIImage(named: addPlaceImage.text!)!)!)
+            newPlaces.iPlaceDetail = addPlaceDetail.text
+            newPlaces.iPlaceChecked = false
+            
+            do {
+                try managedObjectContext.save()
+            } catch {
+                print(error)
+            }
+        }
      }
-     */
+    
+    func alertError(_ title: String, msg: String) {
+        let alert    = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        
+        alert.addAction(okButton)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+ 
 
 }
