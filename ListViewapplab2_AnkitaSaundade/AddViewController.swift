f73592cd11a6896ee4831650fe3eae43c054e6ed
PatriciaCR <PatriciaCR@Patricias-MacBook-Pro-3.local>
2017-03-08 15:41:42 -0800

    Fixed Core data

--- ListViewapplab2_AnkitaSaundade/AddViewController.swift
+++ ListViewapplab2_AnkitaSaundade/AddViewController.swift
@@ -22,11 +22,27 @@ class AddViewController: UIViewController {
     
     @IBAction func clickedSave(_ sender: Any) {
         // Check if name is empty
-        if (addPlaceName.hasText) {
+        if (!addPlaceName.hasText) {
             self.alertError("Unable to save", msg: "Item name can't be blank.")
         }
 
-        performSegue(withIdentifier: "saveSegue", sender: sender)
+        let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).managedObjectContext
+        
+        newPlaces = NSEntityDescription.insertNewObject(forEntityName: "PlaceObject", into: managedObjectContext) as! PlaceObjectMO
+        
+        newPlaces.iPlaceName = addPlaceName.text
+        newPlaces.iPlaceImage = NSData(data: UIImagePNGRepresentation(UIImage(named: addPlaceImage.text!)!)!)
+        newPlaces.iPlaceDetail = addPlaceDetail.text
+        newPlaces.iPlaceChecked = false
+        
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print(error)
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+
     }
     
     
@@ -43,33 +59,6 @@ class AddViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    
-    
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-        
-        if segue.identifier == "saveSegue" {
-            let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).managedObjectContext
-            
-            newPlaces = NSEntityDescription.insertNewObject(forEntityName: "PlaceObject", into: managedObjectContext) as! PlaceObjectMO
-            
-            newPlaces.iPlaceName = addPlaceName.text
-            newPlaces.iPlaceImage = NSData(data: UIImagePNGRepresentation(UIImage(named: addPlaceImage.text!)!)!)
-            newPlaces.iPlaceDetail = addPlaceDetail.text
-            newPlaces.iPlaceChecked = false
-            
-            do {
-                try managedObjectContext.save()
-            } catch {
-                print(error)
-            }
-        }
-     }
     
     func alertError(_ title: String, msg: String) {
         let alert    = UIAlertController(title: title, message: msg, preferredStyle: .alert)
